961685c66597f6422b4f8852d9559434a298f5a3
Eric-chagas <eric_chagas12@outlook.com>
2023-01-15 18:57:43 -0300

    Init db script
    Co-authored-by: savioc2 <saviocunha61@gmail.com>
    Co-authored-by: Gustavo Barbosa <gustavobarbosa7162@gmail.com>

--- initdb.sql
+++ initdb.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS test (
     poder VARCHAR(50),
     PRIMARY KEY(test_id)
 );
+
+CREATE TABLE IF NOT EXISTS chooselife (
+    chooselife_id INT GENERATED ALWAYS AS IDENTITY,
+    test_id INT,
+    seila VARCHAR(50),
+    arroz INT,
+    telefone VARCHAR(50),
+    PRIMARY KEY(chooselife_id),
+    CONSTRAINT fk_test
+      FOREIGN KEY(test_id) 
+	    REFERENCES test(test_id)
+);
+
