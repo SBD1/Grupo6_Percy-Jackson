7b78f21592e8f9eb6b664c2dc66cbcaaf522dc7a
savioc2 <saviocunha61@gmail.com>
2023-01-24 00:19:49 -0300

    update logica do jogo
    Co-authored-by: Gabriel Luiz <gabriel.luiz77888@gmail.com>
    Co-authored-by: Eric Chagas <eric_chagas12@outlook.com>

---
+++ src/database/dml.sql
@@ -0,0 +1,7 @@
+--- Nivel
+insert into Nivel(descricao) values ('Facil');
+insert into Nivel(descricao) values ('Medio');
+insert into Nivel(descricao) values ('Dificil');
+
+--- Item
+insert into Item(nome, descricao, tipo) values ('Espada', 'Tira 20 de HP na batalha', 'Arma');
