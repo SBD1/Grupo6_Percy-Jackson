3f057ba3808d948668817bd89c48a1ed105534b8
Giulia Alcantara <alcantaragiubs@gmail.com>
2023-01-30 22:40:05 -0300

    Inventario Repository

---
+++ src/repositories/inventario_repository.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+from database.database_handler import DatabaseHandler
+from model.inventario import Inventario
+
+
+class UserRepository:
+    def __init__(self) -> None:
+        self.db = DatabaseHandler()
+
+    def saveUser(self, user: Inventario) -> None:
+        with self.db.connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO public.Inventario(tamanho_inventario, momento_coleta_Item, id_item, id_jogador) VALUES(%s, %s, %s, %s)",
+                    [user.tamanho_inventario, user.momento_coleta_Item, user.id_item, user.id_jogador]
+                )
+
+    def updateUser(self, user: Inventario) -> None:
+        assert user.id is not None
+        with self.db.connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                     "UPDATE INTO public.Inventario(tamanho_inventario, momento_coleta_Item, id_item, id_jogador) VALUES(%s, %s, %s, %s)",
+                    [user.tamanho_inventario, user.momento_coleta_Item, user.id_item, user.id_jogador]  
+                 )
+    
+    def deleteUser(self, id) -> None:
+        with self.db.connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "DELETE FROM public.Jogador WHERE id = %s",
+                    [id]
+                )
+    
+    def findUserById(self, id) -> Optional[Inventario]: 
+        with self.db.connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                   "SELECT tamanho_inventario, momento_coleta_Item, id_item, id_jogador, id FROM public.Inventario WHERE id = %s",
+                    [id]
+                )
+                result = cursor.fetchone()
+    
+        if result is None:
+            print(f'Inventario com id {id} não encontrado!')
+            return None
+        
+        user = Inventario(*result)
+        
+        return user
+    
+    def findAll(self) -> list[Inventario]: 
+        with self.db.connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                     "SELECT tamanho_inventario, momento_coleta_Item, id_item, id_jogador, id FROM public.Inventario WHERE id = %s",
+                      )
+                result = cursor.fetchall()
+        
+        users = [Inventario(*row) for row in result]
+        
+        return users    
