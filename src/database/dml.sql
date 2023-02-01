5240f05c451c48fb8afeb166fd3350be6a68c661
Gustavo Barbosa <gustavobarbosa7162@gmail.com>
2023-02-01 01:04:01 -0300

    change database population
    Co-authored-by: Giulia Alcantara <alcantaragiubs@gmail.com>

--- src/database/dml.sql
+++ src/database/dml.sql
@@ -1,7 +1,7 @@
 --- Nivel
-insert into Nivel(descricao) values ('Iniciante');
-insert into Nivel(descricao) values ('Intermediario');
-insert into Nivel(descricao) values ('Avançado');
+insert into Nivel(id, descricao) values (1, 'Iniciante');
+insert into Nivel(id, descricao) values (2, 'Intermediario');
+insert into Nivel(id, descricao) values (3, 'Avançado');
 
 --- Sala
 insert into Sala(id, nome, descricao, id_nivel, destinos) values (1, '1', 'Sala com monstros e sem item', '1', ARRAY [2, 0, 4, 0]);
@@ -14,37 +14,6 @@ insert into Sala(id, nome, descricao, id_nivel, destinos) values (7, '7', 'Sala 
 insert into Sala(id, nome, descricao, id_nivel, destinos) values (8, '8', 'Sala com monstros e com todos os itens', '1', ARRAY [9, 7, 0, 5]); --Baú
 insert into Sala(id, nome, descricao, id_nivel, destinos) values (9, '9', 'Sala vitoriosa', '1', ARRAY [0, 8, 0, 6]); --NPC Boss
 
---- Item
-insert into Item(id, nome, descricao) values (1, 'Anaklusmos', 'Espada com poder de dano de 20 de HP na batalha');
-insert into Item(id, nome, descricao) values (2, 'Velocino de Ouro', 'Recupera a quantidade de HP ');
-insert into Item(id, nome, descricao) values (3, 'Tênis Alados de Hermes', 'Teletransporte durante da partida ');
-insert into Item(id, nome, descricao) values (5, 'Flor de Lótus', 'Poder de dano de 15 de HP na batalha');
-insert into Item(id, nome, descricao) values (6, 'Ivlivs', 'Poder de dano de 10 de HP na batalha');
-
-insert into Item(id, nome, descricao) values (4, 'Código', 'Esse código será utilizado para desbloquear o próximo nível. Guarde-o com cuidado!');
-
--- --- Arma 
--- insert into Arma(nome, descricao, multi_ataque) values ();
-
--- --- magico 
--- insert into Magico(nome, descricao, multi_vida, teletransporte) values ();
-
---- codigo 
-insert into Codigo(code, id_item) values ('303', 4); 
--- insert into Codigo(nome, descricao) values ('175',"Esse código será utilizado para desbloquear o próximo nível. Guarde-o com cuidado!"); 
--- insert into Codigo(nome, descricao) values ('12', "Esse código será utilizado para desbloquear o próximo nível. Guarde-o com cuidado!"); 
-
---- Bau
-insert into Bau(id_item, id_sala) values(6, 2);
-insert into Bau(id_item, id_sala) values(4, 5);
-insert into Bau(id_item, id_sala) values(2, 5);
-insert into Bau(id_item, id_sala) values(1, 8);
-insert into Bau(id_item, id_sala) values(3, 8);
-insert into Bau(id_item, id_sala) values(5, 8);
-
-
-
-
 /* insert into Sala(nome, descricao, id_nivel, destinos) values ('10', 'Sala vitoriosa', '2', ARRAY [2, 3]);
 insert into Sala(nome, descricao, id_nivel, destinos) values ('11', 'Sala com monstros e sem item', '3', ARRAY [2, 3]);
 insert into Sala(nome, descricao, id_nivel, destinos) values ('12', 'Sala com monstros com alguns itens', '3', ARRAY [2, 3]);
@@ -53,6 +22,38 @@ insert into Sala(nome, descricao, id_nivel, destinos) values ('14', 'Sala vitori
 insert into Sala(nome, descricao, id_nivel, destinos) values ('15', 'Sala vitoriosa', '3', ARRAY [2, 3]);
 */
 
+--- Item
+insert into Item(id, nome, descricao) values (1, 'Código', 'Esse código será utilizado para desbloquear o próximo nível. Guarde-o com cuidado!');
+insert into Item(id, nome, descricao) values (2, 'Código', 'Esse código será utilizado para desbloquear o próximo nível. Guarde-o com cuidado!');
+insert into Item(id, nome, descricao) values (3, 'Código', 'Esse código será utilizado para desbloquear o próximo nível. Guarde-o com cuidado!');
+insert into Item(id, nome, descricao) values (4, 'Anaklusmos', 'Espada com poder de dano de 20 de HP na batalha');
+insert into Item(id, nome, descricao) values (5, 'Flor de Lótus', 'Poder de dano de 15 de HP na batalha');
+insert into Item(id, nome, descricao) values (6, 'Ivlivs', 'Poder de dano de 10 de HP na batalha');
+insert into Item(id, nome, descricao) values (7, 'Velocino de Ouro', 'Recupera a quantidade de HP ');
+insert into Item(id, nome, descricao) values (8, 'Tênis Alados de Hermes', 'Teletransporte durante da partida ');
+
+
+-- --- Arma 
+insert into Arma(multi_ataque, id_item) values (20, 4);
+insert into Arma(multi_ataque, id_item) values (15, 5);
+insert into Arma(multi_ataque, id_item) values (10, 6);
+
+-- --- magico 
+insert into Magico(multi_vida, teletransporte, id_item) values (100, false, 7);
+insert into Magico(multi_vida, teletransporte, id_item) values (0, true, 8);
+
+--- codigo 
+insert into Codigo(code, id_item) values ('303', 1); 
+insert into Codigo(code, id_item) values ('175',2); 
+insert into Codigo(code, id_item) values ('12', 3); 
+
+--- Bau
+insert into Bau(id_item, id_sala) values(6, 2);
+insert into Bau(id_item, id_sala) values(4, 5);
+insert into Bau(id_item, id_sala) values(2, 5);
+insert into Bau(id_item, id_sala) values(1, 8);
+insert into Bau(id_item, id_sala) values(3, 8);
+insert into Bau(id_item, id_sala) values(5, 8);
 
 /* --- Jogador
 insert into Jogador(progresso, vida, logar, ataque, defesa, id_sala, id_nivel) values();
@@ -66,16 +67,20 @@ insert into Inventario(tamanho_inventario, momento_coleta_Item, id_item, id_joga
 insert into NPC(id, nome, fala, id_sala) values(1,'Harpia','Morra, meu bem!', '3');
 insert into NPC(id, nome, fala, id_sala) values(2,'Medusa','Deixe-me ver seus olhos. Eu soube que são mais azuis que o Mar Circassiano', '4');
 
+insert into NPC(id, nome, fala, id_sala) values(3,'Minotauro','Rrrrrrr, Eu vou te matar!', '8');
+
 --- Inimigo
 insert into Inimigo(id, vida, ataque, id_npc) values(1, 20 , 5, 1); 
 insert into Inimigo(id, vida, ataque, id_npc) values(2, 20 , 5, 2); 
+
+insert into Inimigo(id, vida, ataque, id_npc) values(3, 20 , 5, 3); 
 
 --- Comum
 insert into Comum(id_inimigo) values(1); 
 insert into Comum(id_inimigo) values(2); 
 
 -- --- Boss
--- insert into Boss(multi_vida, multi_ataque, id_nivel) values();
+insert into Boss(multi_vida, multi_ataque, id_nivel, id_inimigo) values(0.8, 0.8, 1, 3);
 
 -- insert into NPC(nome,fala, id_sala) values('Chiron','', '1');
 -- insert into NPC(nome,fala, id_sala) values('Poseidon','', '2');
